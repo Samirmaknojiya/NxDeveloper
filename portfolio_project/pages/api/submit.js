f5bc4aee8c164fad5308d82e0d5d9670ebf03563
sameer <maknojiyasameer1002@gmail.com>
2023-08-12 00:57:30 +0530

    first commit

---
+++ portfolio_project/pages/api/submit.js
@@ -0,0 +1,30 @@
+import { database} from '../firebase'; // Update the import path based on your project structure
+import { ref, push, set } from 'firebase/database';
+
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, email, phone, course, college } = req.body;
+
+    try {
+      const usersRef = ref(database, 'users'); // Create a reference using ref method
+      const newUserRef = push(usersRef); // Push a new child node to the reference
+
+      await set(newUserRef, {
+        name,
+        email,
+        phone,
+        course,
+        college,
+      });
+
+      return res.status(200).json({ message: 'Form data submitted successfully' });
+    } catch (error) {
+      return res.status(500).json({ error: 'An error occurred while submitting the form' });
+    }
+  } else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
+
+
---
+++ portfolio_project/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screen:{
+      'sm': '380px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ...
+    },
+    // colors:{
+    //   "sameer": "#b3b1bd",
+
+    // },
+      extend: {
+      fontFamily: {
+        burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
